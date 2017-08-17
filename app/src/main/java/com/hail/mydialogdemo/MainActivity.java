6df899061ab867f67b2e0f02c82acd8356c8f89d
wz2793312458 <wz2793312458@163.com>
2017-08-17 18:27:02 +0800

    Initial commit

---
+++ app/src/main/java/com/hail/mydialogdemo/MainActivity.java
@@ -0,0 +1,161 @@
+package com.hail.mydialogdemo;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import cn.pedant.SweetAlert.SweetAlertDialog;
+
+public class MainActivity extends AppCompatActivity {
+    int i = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.btn1, R.id.btn2, R.id.btn3, R.id.btn4, R.id.btn5, R.id.btn6, R.id.btn7, R.id.btn8, R.id.btn9, R.id.btn10, R.id.btn11})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btn1://显示Material进度样式     final int i = 0;
+                SweetAlertDialog pDialog1 = new SweetAlertDialog(this, SweetAlertDialog.PROGRESS_TYPE);
+                pDialog1.getProgressHelper().setBarColor(Color.parseColor("#A5DC86"));
+                pDialog1.setTitleText("Loading");
+                pDialog1.setCancelable(false);
+                pDialog1.setCanceledOnTouchOutside(true);
+                pDialog1.show();
+                break;
+            case R.id.btn2://只显示标题
+                new SweetAlertDialog(this)
+                        .setTitleText("Here's a message!")
+                        .show();
+                break;
+            case R.id.btn3://显示标题和内容
+                new SweetAlertDialog(this)
+                        .setTitleText("Here's a message!")
+                        .setContentText("It's pretty, isn't it?")
+                        .show();
+                break;
+            case R.id.btn4://显示异常样式
+                new SweetAlertDialog(this, SweetAlertDialog.ERROR_TYPE)
+                        .setTitleText("Oops...")
+                        .setContentText("Something went wrong!")
+                        .show();
+                break;
+            case R.id.btn5://显示警告样式
+                new SweetAlertDialog(this, SweetAlertDialog.WARNING_TYPE)
+                        .setTitleText("Are you sure?")
+                        .setContentText("Won't be able to recover this file!")
+                        .setConfirmText("Yes,delete it!")
+                        .show();
+                break;
+            case R.id.btn6://显示成功完成样式
+                new SweetAlertDialog(this, SweetAlertDialog.SUCCESS_TYPE)
+                        .setTitleText("Good job!")
+                        .setContentText("You clicked the button!")
+                        .show();
+                break;
+            case R.id.btn7://自定义头部图像
+                new SweetAlertDialog(this, SweetAlertDialog.CUSTOM_IMAGE_TYPE)
+                        .setTitleText("Sweet!")
+                        .setContentText("Here's a custom image.")
+                        .setCustomImage(R.mipmap.ic_launcher)
+                        .show();
+                break;
+            case R.id.btn8://确认事件绑定
+                new SweetAlertDialog(this, SweetAlertDialog.WARNING_TYPE)
+                        .setTitleText("Are you sure?")
+                        .setContentText("Won't be able to recover this file!")
+                        .setConfirmText("Yes,delete it!")
+                        .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                            @Override
+                            public void onClick(SweetAlertDialog sDialog) {
+                                sDialog.dismissWithAnimation();
+                            }
+                        })
+                        .show();
+                break;
+            case R.id.btn9://显示取消按钮及事件绑定
+                new SweetAlertDialog(this, SweetAlertDialog.WARNING_TYPE)
+                        .setTitleText("Are you sure?")
+                        .setContentText("Won't be able to recover this file!")
+                        .setCancelText("No,cancel plx!")
+                        .setConfirmText("Yes,delete it!")
+                        .showCancelButton(true)
+                        .setCancelClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                            @Override
+                            public void onClick(SweetAlertDialog sDialog) {
+                                sDialog.cancel();
+                            }
+                        })
+                        .show();
+                break;
+            case R.id.btn10://确认后切换对话框样式
+                new SweetAlertDialog(this, SweetAlertDialog.WARNING_TYPE)
+                        .setTitleText("Are you sure?")
+                        .setContentText("Won't be able to recover this file!")
+                        .setConfirmText("Yes,delete it!")
+                        .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                            @Override
+                            public void onClick(SweetAlertDialog sDialog) {
+                                sDialog
+                                        .setTitleText("Deleted!")
+                                        .setContentText("Your imaginary file has been deleted!")
+                                        .setConfirmText("OK")
+                                        .setConfirmClickListener(null)
+                                        .changeAlertType(SweetAlertDialog.SUCCESS_TYPE);
+                            }
+                        })
+                        .show();
+                break;
+            case R.id.btn11://环形进度条变换颜色
+                final SweetAlertDialog pDialog = new SweetAlertDialog(this, SweetAlertDialog.PROGRESS_TYPE).setTitleText("Loading");
+                pDialog.show();
+                pDialog.setCancelable(false);
+                new CountDownTimer(800 * 7, 800) {
+                    public void onTick(long millisUntilFinished) {
+// you can change the progress bar color by ProgressHelper every 800 millis
+
+                        i++;
+                        switch (i) {
+                            case 0:
+                                pDialog.getProgressHelper().setBarColor(getResources().getColor(R.color.blue_btn_bg_color));
+                                break;
+                            case 1:
+                                pDialog.getProgressHelper().setBarColor(getResources().getColor(R.color.material_deep_teal_50));
+                                break;
+                            case 2:
+                                pDialog.getProgressHelper().setBarColor(getResources().getColor(R.color.success_stroke_color));
+                                break;
+                            case 3:
+                                pDialog.getProgressHelper().setBarColor(getResources().getColor(R.color.material_deep_teal_20));
+                                break;
+                            case 4:
+                                pDialog.getProgressHelper().setBarColor(getResources().getColor(R.color.material_blue_grey_80));
+                                break;
+                            case 5:
+                                pDialog.getProgressHelper().setBarColor(getResources().getColor(R.color.warning_stroke_color));
+                                break;
+                            case 6:
+                                pDialog.getProgressHelper().setBarColor(getResources().getColor(R.color.success_stroke_color));
+                                break;
+                        }
+                    }
+
+                    public void onFinish() {
+                        i = -1;
+                        pDialog.setTitleText("Success!")
+                                .setConfirmText("OK")
+                                .changeAlertType(SweetAlertDialog.SUCCESS_TYPE);
+                    }
+                }.start();
+                break;
+        }
+    }
+}
